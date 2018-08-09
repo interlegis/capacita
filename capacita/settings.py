038053392a74e3295e750cfb29b81ab6ae7c888b
bruno <brunocordeiro180@gmail.com>
2018-08-09 15:39:09 -0300

    Capacita 1.0.0-3

--- capacita/settings.py
+++ capacita/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'cl7h5!4x-4nyu*9gv1n8voo@m4$=gmx%xh6sxu!8tuifob*vk!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
