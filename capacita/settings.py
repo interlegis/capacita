e9b01116e04cb0ee9dc93be13d427e761e178416
bruno <brunocordeiro180@gmail.com>
2018-08-09 16:05:18 -0300

    Capacita 1.0.0-4

--- capacita/settings.py
+++ capacita/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [ #components
     'novo_capacita',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
