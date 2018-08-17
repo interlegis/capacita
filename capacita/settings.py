984418f3fc4741f133f41aff0271158221e7d6be
bruno cordeiro <brunocordeiro180@gmail.com>
2018-08-17 17:28:32 -0300

    Capacita 1.0.0-14

--- capacita/settings.py
+++ capacita/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'capacita.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'capacita',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': os.environ.get('DATABASE_PORT', ''),
     }
 }
 
