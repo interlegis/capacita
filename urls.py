49ecc7b8e7bb7acf637e24ae41624957581ef3a2
bruno <brunocordeiro180@gmail.com>
2018-09-06 16:10:32 -0300

    import django_cas_ng em urls

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+import django_cas_ng.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
