629ab52d100d7c3e88af16dfcc2ba65b23f501ca
Matheus456 <matheus456456@gmail.com>
2018-12-14 17:02:48 -0200

    Reparando um erro e colocando migrações

--- capacitaApp/models.py
+++ capacitaApp/models.py
@@ -1,10 +1,6 @@
 from django.db import models
-from django.utils import timezone
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
 
+from django.contrib.auth.models import User
 from plano.models import Plano_Capacitacao
 from orgao.models import Orgao
 from areas.models import Area_Conhecimento
