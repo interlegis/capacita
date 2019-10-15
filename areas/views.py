8693f4ed2a01f2b2e2201d9da4291e9d55cfe985
matheus456 <matheus456456@gmail.com>
2019-10-15 10:59:21 -0300

    Corrigindo erro de habilitar área de conhecimento e refatorando código

--- areas/views.py
+++ areas/views.py
@@ -3,74 +3,63 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 from capacitaApp.views import is_admin, is_gestor
+from capacita.decorators import admin_required
 
 @login_required
+@admin_required
 def areas(request):
-    if is_admin(request)['is_admin']:
-        areas = Area_Conhecimento.objects.all();
-        return render(request, 'areas.html', { 'areas' : areas })
-    else:
-        return render(request, 'error.html')
+    areas = Area_Conhecimento.objects.all();
+    return render(request, 'areas.html', { 'areas' : areas })
 
 @login_required
+@admin_required
 def area_delete(request, pk):
-    if is_admin(request)['is_admin']:
-        try:
-            Area_Conhecimento.objects.filter(pk=pk).delete()
-            areas = Area_Conhecimento.objects.all();
-            return redirect("areas")
-        except Exception as e:
-            print(e)
-            areas = Area_Conhecimento.objects.all();
-            return render(request, 'deleteError.html', {'url': 'areas.html', 'areas': areas})
-    else:
-        return render(request, 'error.html')
+    try:
+        Area_Conhecimento.objects.filter(pk=pk).delete()
+        areas = Area_Conhecimento.objects.all();
+        return redirect("areas")
+    except Exception as e:
+        print(e)
+        areas = Area_Conhecimento.objects.all();
+        return render(request, 'deleteError.html', {'url': 'areas.html', 'areas': areas})
 
 @login_required
+@admin_required
 def area_invisible(request, pk):
-    if is_admin(request)['is_admin']:
-        Area_Conhecimento.objects.filter(pk=pk).update(ind_excluido=True)
-        return redirect("areas")
-    else:
-        return render(request, 'error.html')
+    Area_Conhecimento.objects.filter(pk=pk).update(ind_excluido=True)
+    return redirect("areas")
 
 @login_required
+@admin_required
 def area_visible(request, pk):
-    if is_admin(request)['is_admin']:
-        Modalidade_Treinamento.objects.filter(pk=pk).update(ind_excluido=False)
-        return redirect("areas")
-    else:
-        return render(request, 'error.html')
+    Area_Conhecimento.objects.filter(pk=pk).update(ind_excluido=False)
+    return redirect("areas")
 
 @login_required
+@admin_required
 def area_edit(request,id):
-    admin = is_admin(request)['is_admin']
     area = get_object_or_404(Area_Conhecimento, pk=id)
-    if request.method == 'POST' and admin:
+    if request.method == 'POST':
         form = AreaConhecimentoForm(request.POST, instance=area, initial={'ind_excluido' : False})
         if form.is_valid():
             area = form.save()
             return redirect("areas")
         else:
             return render(request, 'area_edit.html', {'form' : form})
-    elif request.method != 'POST' and admin:
+    else:
         form = AreaConhecimentoForm(instance=area)
         return render(request, 'area_edit.html', {'form' : form})
-    else:
-        return render(request, 'error.html')
 
 @login_required
+@admin_required
 def area_new(request):
-    admin = is_admin(request)['is_admin']
-    if request.method == 'POST' and admin:
+    if request.method == 'POST':
         form = AreaConhecimentoForm(request.POST)
         if form.is_valid():
             area = form.save()
             return redirect("areas")
         else:
             return render(request, 'area_edit.html', {'form' : form})
-    elif request.method != 'POST' and admin:
+    else:
         form = AreaConhecimentoForm()
         return render(request, 'area_edit.html', {'form' : form})
-    else:
-        return render(request, 'error.html')
